d38f8f568cc8ea91cc30c38a2722d598a8a3d9fe
Evan03 <Evan.onihotmail.com>
2023-02-07 00:03:36 -0500

    added operations functions and handlers for buttons

--- functions.js
+++ functions.js
@@ -1,113 +1,51 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn-num');
+const operators = document.querySelectorAll('.btn-op');
+const clear = document.querySelector('.btn-clear');
+const equals = document.querySelector('.btn-eq');
+const deci = document.querySelector('.btn-deci')
 
- 
-    let clear = document.querySelector('.btn-clear');
-    let numbers = document.querySelectorAll('.btn-num');
-    let operators = document.querySelectorAll('.btn-op');
-    let equal = document.querySelector('.btn-eq');
-    let display = document.querySelector('.screen')
-    let previousScreen = document.querySelector('.previous')
-    let currentScreen = document.querySelector('.current')
-
-
-// adding button functionality to the buttons
-
-let currentVal='';
-let currentOp =''
-let previousVal ='';
-
-//Current screen
+// making operations
+let num1='';
+let num2='';
+let result ='';
 
 
+function add(num1, num2 ,result){
+    result = parseFloat(num1)+parseFloat(num2);
+    num1=result;
+}
 
-//Previous Screen
+function sub(num1, num2 ,result){
+    result = parseFloat(num1)-parseFloat(num2);
+    num1=result;
+}
+
+function multiply(num1, num2 ,result){
+    result = parseFloat(num1)*parseFloat(num2);
+    num1=result;
+}
+
+function divide(num1, num2 ,result){
+    if(num2===0) return 'LMAOO'
+    result= parseFloat(num1)/parseFloat(num2);
+    num1=result;
+}
 
 
-//Operator Buttons
 
 
-operators.forEach(operator =>operator.addEventListener('click',function(e){
-       handleOperator(e.target.textContent)
-       previousScreen.textContent= previousVal + ' ' + currentOp;
-    currentScreen.textContent = currentVal;
+//Adding actionlistners to buttons
+// throws error cannot set display to null fix 
+buttons.forEach((button) =>button.addEventListener('click',function(e){
+         handleNumber(e.target.textContent)
+         display.textContent = num1;
 }))
 
 
-numbers.forEach((number)=> number.addEventListener('click', function(e){
-    handleNums(e.target.textContent)
-   currentScreen.textContent=currentVal;
-}))
 
-// allows for button display
-function handleNums(nums){
-    if(currentVal.length <=6){
-        currentVal+=nums
-    }
-  
-}
+//Helper method to append numbers to display
 
-// allows for operatror display
-
- function handleOperator(operator){
-   //  if(currentOp<=1){
-        currentOp = operator
-        previousVal = currentVal
-        currentVal =''
-    
-}
-
-
-clear.addEventListener('click', function(e){
-      previousVal='';
-      currentVal='';
-      operator='';
-      previousScreen.textContent = currentVal;
-      currentScreen.textContent= currentVal;
-
-
-})
-
-
-equal.addEventListener('click', function(){
-    calculate()
-    previousScreen.textContent = '';
-    currentScreen.textContent=previousVal;
-})
-
-//operate function 
-
-
-
-
-function calculate(){
-    previousVal = Number(previousVal);
-    currentVal = Number(currentVal);
-    switch(currentOp){
-        case '+': 
-        previousVal+= currentVal; 
-        break;
-
-        case'-': 
-        previousVal-=currentVal;
-        break;
-        
-        case'*': 
-       previousVal*= currentVal;
-        break;
-
-        case'/': 
-        previousVal/= currentVal;
-        break;
-        
-        default:
-     
-    }
-    previousVal = Round(previousVal)
-    previousVal = previousVal.toString()
-    currentVal = currentVal.toString();
-
-
-}
-
-function Round(nums){
-   return Math.round(nums*1000)/1000
+function handleNumber(num){
+    num1 +=num;
 }
